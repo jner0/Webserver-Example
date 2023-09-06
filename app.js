36fc4f3bb7c77c620bfebc032a6c2860875f9902
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-09-05 23:30:42 -0500

    Handlebars

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.set("view engine", "hbs");
+
 //Servir contenido estatico
 
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.get("/generic", (req, res) => {
   res.sendFile(__dirname + "/public/generic.html");
